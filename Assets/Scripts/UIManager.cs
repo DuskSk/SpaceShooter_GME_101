76278027e48f243e8af81167f1fbf7106bcbad4e
Lucas Matias <lucasdmatias@hotmail.com>
2025-01-27 00:15:28 -0300

    Added Game Manager script and Main Menu logic

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -10,11 +11,28 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Player _player;
     [SerializeField] private Sprite[] _livesSpritesList;
     [SerializeField] private Image _livesImage;
-    // Start is called before the first frame update
+    [SerializeField] private TMP_Text _gameOverText;
+    [SerializeField] private TMP_Text _reloadText;    
+    private GameManager _gameManager;
+    
+
     void Start()
     {
         _scoreText.text = $"Score: {0}";
-    }   
+        _gameOverText.gameObject.SetActive(false);
+        _reloadText.gameObject.SetActive(false);
+        _gameManager = GameObject.FindWithTag("Game_Manager").GetComponent<GameManager>();
+
+        if (_gameManager == null)
+        {
+            Debug.Log("Game Manager is NULL");
+        }
+
+
+    }
+
+
+
 
     public void UpdateScoreText()
     {
@@ -24,5 +42,33 @@ public class UIManager : MonoBehaviour
     public void UpdateLivesImage(int currentPlayerLives)
     {
         _livesImage.sprite = _livesSpritesList[currentPlayerLives];
+
+        if (currentPlayerLives == 0) 
+        {
+            StartGameOverSequence();                       
+            
+        }
+
     }
+
+    private void StartGameOverSequence()
+    {
+        StartCoroutine(GameOverFlickerRoutine());
+        _reloadText.gameObject.SetActive(true);
+        _gameManager.GameOver();
+    }
+
+
+    private IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.7f);
+            _gameOverText.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.7f);            
+        }
+    }
+
+   
 }
